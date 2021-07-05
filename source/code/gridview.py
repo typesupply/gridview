2d7538d5dc0567e3cebf6b908b3577aa168de5e7
Tal Leming <tal@typesupply.com>
2021-07-05 16:16:55 -0400

    Trying CATiledLayer.

--- source/code/gridview.py
+++ source/code/gridview.py
@@ -1,4 +1,5 @@
 import math
+import AppKit
 import ezui
 import merz
 from mojo.UI import getDefault
@@ -13,32 +14,28 @@ from mojo.subscriber import Subscriber, registerGlyphEditorSubscriber
 
 identifierStub = "com.typesupply.GridView."
 
-def alphaColor(color, alpha):
-    r, g, b, a = color
-    return (r, g, b, alpha)
-
-backgroundColor = getDefault("glyphViewBackgroundColor")
-gridColor = getDefault("glyphViewGridColor")
-textColor = alphaColor(gridColor, 1)
-grid1Color = alphaColor(gridColor, 0.1)
-grid10Color = alphaColor(gridColor, 0.3)
-grid100Color = alphaColor(gridColor, 0.6)
-
-textSettings = dict(
-    padding=(5, 2),
-    horizontalAlignment="center",
-    verticalAlignment="center",
-    backgroundColor=backgroundColor,
-    fillColor=textColor,
-    figureStyle="tabular",
-    pointSize=12 ,
-    weight="light"
-)
+# def alphaColor(color, alpha):
+#     r, g, b, a = color
+#     return (r, g, b, alpha)
+#
+# textSettings = dict(
+#     padding=(5, 2),
+#     horizontalAlignment="center",
+#     verticalAlignment="center",
+#     backgroundColor=backgroundColor,
+#     fillColor=textColor,
+#     figureStyle="tabular",
+#     pointSize=12 ,
+#     weight="light"
+# )
 
 
-class GridView(Subscriber):
 
-    debug = False
+
+
+class GridView2(Subscriber):
+
+    debug = True
 
     def getGlyph(self):
         editor = self.getGlyphEditor()
@@ -62,32 +59,96 @@ class GridView(Subscriber):
             location="background",
             clear=True
         )
-        container.clearSublayers()
-        self.buildGrids()
+        self.loadDefaults()
+        self.buildBases()
+
+    # Delegate Actions
+    # ----------------
 
     def started(self):
-        self.zoomGrid()
+        pass
+
+    def preferencesChanged(self, info):
+        self.loadDefaults()
+        self.buildImageContainers()
+        self.populateImageContainers()
+
+    def glyphEditorDidSetGlyph(self, info):
+        self.buildImageContainers()
+        self.populateImageContainers()
 
     def glyphEditorDidScale(self, info):
-        self.zoomGrid()
+        self.populateImageContainers()
 
-    def buildGrids(self):
+    def glyphEditorGlyphDidChangeMetrics(self, info):
+        self.buildImageContainers()
+        self.populateImageContainers()
+
+    # Defaults
+    # --------
+
+    def loadDefaults(self):
+        self.gridBuffer = getDefault("glyphViewGridBorder")
+        self.backgroundColor = getDefault("glyphViewBackgroundColor")
+        self.gridColor = getDefault("glyphViewGridColor")
+        self.gridColor = (1, 0, 0, 1)
+        self.textColor = self.gridColor
+        self.grid1Color = scaleColorAlpha(self.gridColor, 0.1)
+        self.grid10Color = scaleColorAlpha(self.gridColor, 0.3)
+        self.grid100Color = self.gridColor
+
+    gridBuiltForWidth = None
+    gridBuiltForDescender = None
+    gridBuiltForUPM = None
+    gridBuiltForBuffer = None
+
+    def buildBases(self):
         container = self.container
+        container.clearSublayers()
+        self.grid1Base = container.appendBaseSublayer(
+            name="grid1Base"
+        )
+        self.grid10Base = container.appendBaseSublayer(
+            name="grid10Base"
+        )
+        self.grid100Base = container.appendBaseSublayer(
+            name="grid100Base"
+        )
+
+    def clearBases(self):
+        self.grid1Base.clearSublayers()
+        self.grid10Base.clearSublayers()
+        self.grid100Base.clearSublayers()
+
+    def buildImageContainers(self):
+        container = self.container
+        glyph = self.getGlyph()
+        if glyph is None:
+            self.clearBases()
+            return
         font = self.getFont()
-        upm = font.info.unitsPerEm
-        xMaximum = roundUp(upm * 5, 100)
-        xMinimum = roundDown(-xMaximum, 100)
-        yMaximum = roundUp(upm * 1.5, 100)
-        yMinimum = roundDown(-yMaximum, 100)
-        xTextMinimum = roundDown(upm * -0.75, 100)
-        xTextMaximum = roundUp(upm * 1.5, 100)
-        yTextMinimum = roundDown(upm * -0.5, 100)
-        yTextMaximum = roundUp(upm * 1.25, 100)
+        needRebuild = False
+        if self.gridBuffer != self.gridBuiltForBuffer:
+            needRebuild = True
+        elif glyph.width != self.gridBuiltForWidth:
+            needRebuild = True
+        elif font.info.descender != self.gridBuiltForDescender:
+            needRebuild = True
+        elif font.info.unitsPerEm == self.gridBuiltForUPM:
+            needRebuild = True
+        if not needRebuild:
+            return
+        self.gridBuiltForWidth = glyph.width
+        self.gridBuiltForDescender = font.info.descender
+        self.gridBuiltForUPM = font.info.unitsPerEm
+        self.gridBuiltForBuffer = self.gridBuffer
 
-        width = abs(xMinimum) + xMaximum
-        height = abs(yMinimum) + yMaximum
-
-        # lines
+        xMinimum, xMaximum = sorted((0, glyph.width))
+        xMinimum = roundDown(xMinimum - self.gridBuffer, 100)
+        xMaximum = roundUp(xMaximum + self.gridBuffer, 100)
+        yMinimum = roundDown(font.info.descender - self.gridBuffer, 100)
+        yMaximum = roundUp(font.info.unitsPerEm + self.gridBuffer, 100)
+        self.clearBases()
 
         x1Positions = [
             i for i in range(xMinimum, xMaximum, 1)
@@ -119,91 +180,139 @@ class GridView(Subscriber):
             i for i in range(yMinimum, yMaximum, 100)
         ]
 
-        def drawGrid(layer, xPositions, yPositions):
-            pen = merz.MerzPen()
-            for x in xPositions:
-                pen.moveTo((x, yMinimum))
-                pen.lineTo((x, yMaximum))
-                pen.endPath()
-            for y in yPositions:
-                pen.moveTo((xMinimum, y))
-                pen.lineTo((xMaximum, y))
-                pen.endPath()
-            layer.setPath(pen.path)
+        self.grid100Containers = []
+        for x in x100Positions:
+            for y in y100Positions:
+                layer = self.grid100Base.appendImageSublayer(
+                    position=(x, y),
+                    size=(100, 100),
+                    alignment="center"
+                    # backgroundColor=(1, 0, 0, 0.2),
+                    # borderColor=(1, 0, 0, 1),
+                    # borderWidth=1
+                )
+                self.grid100Containers.append(layer)
 
-        grid1PathLayer = container.appendPathSublayer(
-            name="lines.1",
-            strokeColor=grid1Color,
-            strokeWidth=1
-        )
-        drawGrid(grid1PathLayer, x1Positions, y1Positions)
-        grid1PathLayer.getCALayer().setShouldRasterize_(True)
 
-        grid10PathLayer = container.appendPathSublayer(
-            name="lines.10",
-            strokeColor=grid10Color,
-            strokeWidth=1
-        )
-        drawGrid(grid10PathLayer, x10Positions, y10Positions)
-        grid10PathLayer.getCALayer().setShouldRasterize_(True)
 
-        grid100PathLayer = container.appendPathSublayer(
-            name="lines.100",
-            strokeColor=grid100Color,
-            strokeWidth=1
-        )
-        drawGrid(grid100PathLayer, x100Positions, y100Positions)
-        grid100PathLayer.getCALayer().setShouldRasterize_(True)
+#
+#         # lines
+#
 
-        # text
+#
+#         def drawGrid(layer, xPositions, yPositions):
+#             pen = merz.MerzPen()
+#             for x in xPositions:
+#                 pen.moveTo((x, yMinimum))
+#                 pen.lineTo((x, yMaximum))
+#                 pen.endPath()
+#             for y in yPositions:
+#                 pen.moveTo((xMinimum, y))
+#                 pen.lineTo((xMaximum, y))
+#                 pen.endPath()
+#             layer.setPath(pen.path)
+#
+#         grid1PathLayer = container.appendPathSublayer(
+#             name="lines.1",
+#             strokeColor=grid1Color,
+#             strokeWidth=1
+#         )
+#         drawGrid(grid1PathLayer, x1Positions, y1Positions)
+#         grid1PathLayer.getCALayer().setShouldRasterize_(True)
+#
+#         grid10PathLayer = container.appendPathSublayer(
+#             name="lines.10",
+#             strokeColor=grid10Color,
+#             strokeWidth=1
+#         )
+#         drawGrid(grid10PathLayer, x10Positions, y10Positions)
+#         grid10PathLayer.getCALayer().setShouldRasterize_(True)
+#
+#         grid100PathLayer = container.appendPathSublayer(
+#             name="lines.100",
+#             strokeColor=grid100Color,
+#             strokeWidth=1
+#         )
+#         drawGrid(grid100PathLayer, x100Positions, y100Positions)
+#         grid100PathLayer.getCALayer().setShouldRasterize_(True)
+#
+#         # text
+#
+#         xTextPositions = [
+#             i for i in range(xTextMinimum, xTextMaximum, 100)
+#         ]
+#         yTextPositions = [
+#             i for i in range(yTextMinimum, yTextMaximum, 100)
+#         ]
+#
+#         grid100TextLayer = container.appendBaseSublayer(
+#             name="text.100"
+#         )
+#         grid200TextLayer = container.appendBaseSublayer(
+#             name="text.200"
+#         )
+#         with grid200TextLayer.sublayerGroup():
+#             with grid100TextLayer.sublayerGroup():
+#                 for x in xTextPositions:
+#                     for y in yTextPositions:
+#                         t = None
+#                         if x % 200 or y % 200:
+#                             t = grid100TextLayer
+#                         elif not x % 200 and not y % 200:
+#                             t = grid200TextLayer
+#                         if t is not None:
+#                             l = t .appendTextLineSublayer(
+#                                 position=(x, y),
+#                                 text=f"{x} {y}",
+#                                 **textSettings
+#                             )
 
-        xTextPositions = [
-            i for i in range(xTextMinimum, xTextMaximum, 100)
-        ]
-        yTextPositions = [
-            i for i in range(yTextMinimum, yTextMaximum, 100)
-        ]
-
-        grid100TextLayer = container.appendBaseSublayer(
-            name="text.100"
-        )
-        grid200TextLayer = container.appendBaseSublayer(
-            name="text.200"
-        )
-        with grid200TextLayer.sublayerGroup():
-            with grid100TextLayer.sublayerGroup():
-                for x in xTextPositions:
-                    for y in yTextPositions:
-                        t = None
-                        if x % 200 or y % 200:
-                            t = grid100TextLayer
-                        elif not x % 200 and not y % 200:
-                            t = grid200TextLayer
-                        if t is not None:
-                            l = t .appendTextLineSublayer(
-                                position=(x, y),
-                                text=f"{x} {y}",
-                                **textSettings
-                            )
-
-    def zoomGrid(self):
+    def populateImageContainers(self):
         container = self.container
         scale = container.getCumulativeSublayerTransformation()[0]
-        # 1 unit
-        grid1PathLayer = container.getSublayer("lines.1")
-        grid1PathLayer.setVisible(scale > 5)
-        # 10 units
-        grid10PathLayer = container.getSublayer("lines.10")
-        grid10PathLayer.setVisible(scale > 0.5)
-        # 100 units
-        grid100PathLayer = container.getSublayer("lines.100")
-        grid100PathLayer.setVisible(scale > 0.1)
-        # 100 units text
-        text100PathLayer = container.getSublayer("text.100")
-        text100PathLayer.setVisible(scale > 1.0)
-        # 200 units text
-        text200PathLayer = container.getSublayer("text.200")
-        text200PathLayer.setVisible(scale > 0.3)
+        if scale < 0.1:
+            self.grid100Base.setVisible(False)
+        else:
+            self.make100Image(scale)
+            for layer in self.grid100Containers:
+                layer.setBackgroundColor((1, 1, 0, 0.25))
+                # layer.setImage(self.grid100Image)
+            self.grid100Base.setVisible(True)
+
+    def make100Image(self, unit):
+        size = unit * 100
+        image = AppKit.NSImage.alloc().initWithSize_((size, size))
+        image.lockFocus()
+        AppKit.NSColor.colorWithCalibratedRed_green_blue_alpha_(
+            *self.grid100Color
+        ).set()
+        path = AppKit.NSBezierPath.bezierPathWithRect_(
+            ((0, 0), (size, size))
+        )
+        path.setLineWidth_(1.0 / unit)
+        path.stroke()
+        image.unlockFocus()
+        self.grid100Image = image
+
+    # def zoomGrid(self):
+    #     pass
+        # container = self.container
+        # scale = container.getCumulativeSublayerTransformation()[0]
+        # # 1 unit
+        # grid1PathLayer = container.getSublayer("lines.1")
+        # grid1PathLayer.setVisible(scale > 5)
+        # # 10 units
+        # grid10PathLayer = container.getSublayer("lines.10")
+        # grid10PathLayer.setVisible(scale > 0.5)
+        # # 100 units
+        # grid100PathLayer = container.getSublayer("lines.100")
+        # grid100PathLayer.setVisible(scale > 0.1)
+        # # 100 units text
+        # text100PathLayer = container.getSublayer("text.100")
+        # text100PathLayer.setVisible(scale > 1.0)
+        # # 200 units text
+        # text200PathLayer = container.getSublayer("text.200")
+        # text200PathLayer.setVisible(scale > 0.3)
 
 
 # -----
@@ -218,9 +327,13 @@ def roundDown(n, v=1):
     n -= n % v
     return int(n)
 
+def scaleColorAlpha(color, scale):
+    r, g, b, a = color
+    a *= scale
+    return (r, g, b, a)
 
 # --
 # Go
 # --
 
-registerGlyphEditorSubscriber(GridView)
+registerGlyphEditorSubscriber(GridView2)
